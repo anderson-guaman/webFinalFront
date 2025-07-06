7b56aa58b14a1c8d4a825b42c48e93a5bc56f23e
aguaman123 <anderson.guaman@udla.edu.ec>
2025-07-06 18:13:36 -0500

    Creacion del proyecto

---
+++ src/app/pages/cliente/cliente.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cliente',
+  imports: [],
+  templateUrl: './cliente.component.html',
+})
+export class ClienteComponent {
+
+}
